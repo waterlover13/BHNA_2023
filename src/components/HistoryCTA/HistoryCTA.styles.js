5f012b4931f17523fad23b4918ea9931015c8436
demiData <demi.minjarez@gmail.com>
2021-03-11 23:03:58 -0800

    deleted unused css

--- src/components/HistoryCTA/HistoryCTA.styles.js
+++ src/components/HistoryCTA/HistoryCTA.styles.js
@@ -6,10 +6,9 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   grid-gap: 20px;
-  /* max-width: 1280px;
-  margin: 0 auto; */
+
   margin-top: 20px;
-  /* padding: 0 20px; */
+  
   
 
   @media screen and (min-width: 768px) {
