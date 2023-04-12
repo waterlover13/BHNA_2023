14b78c81a9c40974c21d881870cb63d711f6e959
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2023-04-12 01:05:11 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
 import HistorySection from "../components/HistorySection/HistorySection"
-import JoinBHNA from "../comxponents/JoinBHNA/JoinBHNA"
+import JoinBHNA from "../components/JoinBHNA/JoinBHNA"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
